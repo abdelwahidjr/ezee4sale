ee3967c7f58f53d0389121abe4458661bc3bd5fe
Abd El-Wahid JR <abdelwahid.jr@gmail.com>
2018-12-11 00:17:30 +0200

    minor fixes

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->only('home');
+    }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function test()
+    {
+
+    }
+
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,39 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(PermissionsSeeder::class);
-        $this->call(UserSettingsTableSeeder::class);
-        $this->call(SiteSettingsTableSeeder::class);
-        $this->call(EmployeesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
-        $this->call(FAQsTableSeeder::class);
-        $this->call(AdsTableSeeder::class);
-        $this->call(PopupsTableSeeder::class);
-        $this->call(PagesTableSeeder::class);
-        $this->call(ReviewsTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
-        $this->call(FavoritesTableSeeder::class);
-        $this->call(BrandsTableSeeder::class);
-        $this->call(OutletsTableSeeder::class);
-        $this->call(NotificationsTableSeeder::class);
-        $this->call(NewslettersTableSeeder::class);
-        $this->call(ContactUsTableSeeder::class);
-        $this->call(ContactUsMessagesTableSeeder::class);
-        $this->call(ItemsTableSeeder::class);
-        $this->call(ExtrasTableSeeder::class);
-        $this->call(OrdersTableSeeder::class);
-        $this->call(PromotionsTableSeeder::class);
-        $this->call(CouponsTableSeeder::class);
-        $this->call(GiftCardsTableSeeder::class);
-        $this->call(EventsTableSeeder::class);
-        $this->call(CartsTableSeeder::class);
-        $this->call(AreasTableSeeder::class);
-        $this->call(OutletAreasTableSeeder::class);
-        $this->call(SubOrdersTableSeeder::class);
-        $this->call(FriendshipsTableSeeder::class);
 
-        $this->call(CitiesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(FriendsTableSeeder::class);
+
     }
 }
