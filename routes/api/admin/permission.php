3129afbe7f6c0e8cc060ef614b8eb576733e347d
Abd El-Wahid JR <abdelwahid.jr@gmail.com>
2018-12-11 00:05:43 +0200

    structure minor fixes

--- routes/api/admin/permission.php
+++ routes/api/admin/permission.php
@@ -2,10 +2,10 @@
 
 Route::group(['prefix' => 'permission'] , function ()
 {
-
-
     Route::post('/assign-permission' , 'PermissionController@AssignPermissionForRole');
+
     Route::post('/remove-permission' , 'PermissionController@RemovePermissionFromRole');
+
     Route::get('/all/{permission}' , 'PermissionController@all');
 
 
--- routes/api/public/sms.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'sms'] , function ()
-{
-	Route::get('/send/{phone}/{message}' , 'SMSController@send');
-	Route::get('/status/{messageId}' , 'SMSController@status');
-});
