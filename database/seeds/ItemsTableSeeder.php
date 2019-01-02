37fc02cb62ce1e43da76041bcc3195b95479872f
amr-sameh <amrsameh.develop@gmail.com>
2019-01-02 17:28:40 +0200

    edit item seeder

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -17,24 +17,24 @@ class ItemsTableSeeder extends Seeder
         $catigories_ids = DB::table('categories')->pluck('id');
         $subcatigories_ids = DB::table('sub_categories')->pluck('id');
         if (app()->environment() !== 'production' && App::runningInConsole()) {
-            foreach (range(1, 50) as $index) {
+            foreach ($catigories_ids as $catigories_id) {
+                foreach ($subcatigories_ids as $subcatigories_id) {
 
-                Item::create([
-                    'title' => $faker->name,
-                    'type' => array_random(['ad', 'market']),
-                    'user_id' => rand(1, 50),
-                    'whats_app' => $faker->phoneNumber,
-                    'place' => $faker->address,
-                    'phone' => $faker->phoneNumber,
-                    'category_id' => rand(1, 50),
-                    'due_date'  => $faker->dateTimeBetween('yesterday' , '+10 day'),
-                    'sub_category_id' => rand(1, 50),
-                    'category_id'   => $faker->randomElement( $catigories_ids),
-                    'sub_category_id'   => $faker->randomElement( $subcatigories_ids),
-                    'views_count' => rand(1, 50),
-                    'state' => array_random(['pinned', 'featured', 'none']),
-                    'appear_on_home' => array_random([true, false]),
-                ]);
+                    Item::create([
+                        'title' => $faker->name,
+                        'type' => array_random(['ad', 'market']),
+                        'user_id' => rand(1, 50),
+                        'whats_app' => $faker->phoneNumber,
+                        'place' => $faker->address,
+                        'phone' => $faker->phoneNumber,
+                        'due_date' => $faker->dateTimeBetween('yesterday', '+10 day'),
+                        'category_id' => $faker->randomElement($catigories_ids),
+                        'sub_category_id' => $faker->randomElement($subcatigories_ids),
+                        'views_count' => rand(1, 50),
+                        'state' => array_random(['pinned', 'featured', 'none']),
+                        'appear_on_home' => array_random([true, false]),
+                    ]);
+                }
             }
         }
     }
